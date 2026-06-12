8e6f1716975c67a3fb4f18cde135eb1767466647
ilymammohamed <elimammohamed1@outlook.com>
2026-06-12 16:26:19 +0400

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: false,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'THIS_IS_UNDEFINED') return
+        warn(warning)
+      },
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['recharts'],
+        },
+      },
+    },
+  },
+})
